0dd74341f7d14245920d6f60145a1c9fb64d9002
Ava <109676929+Ayu5259@users.noreply.github.com>
2025-05-23 22:55:28 +0330

    Add files via upload

---
+++ notes.php
@@ -0,0 +1,61 @@
+<?php require_once 'sections/header.php';
+checkLogin(); ?>
+<div class="container-fluid min-vh-100 d-flex flex-column">
+    <div class="row flex-grow-1">
+        <div class="col-lg-2 col-md-3 sidebar">
+            <h2 class="logo">یادداشت ها</h2>
+            <div class="devider"></div>
+            <div class="searchbox">
+                <form action="">
+                    <a href="#"><i class="fas fa-magnifying-glass"></i></a>
+                    <input type="text" placeholder="جستجو">
+                </form>
+            </div>
+            <?php require_once 'sections/menu.php' ?>
+
+            <div class="upgrade">
+                <a href="#" class=""><i class="fas fa-trophy"></i>خرید نسخه کامل</a>
+            </div>
+        </div>
+        <div class="col-lg-10 col-md-9 content g-0">
+            <div class="bg">
+                <a class="profile"><i class="fas fa-user"></i>مشاهده پروفایل</a>
+                <div class="titles">
+                    <h1 class="title">سلام عزیزم</h1>
+                    <h2 class="title">بهترین برنامه، بی‌برنامگیه...</h2>
+                </div>
+            </div>
+
+            <div class="row mycards mx-auto notes">
+                <div class="col-lg-9">
+                    <div class="box">
+                        <h2><i class="fas fa-calendar-day"></i>همه یادداشت ها</h2>
+                        <ul class="list">
+                            <li><a href="#"><i class="fas fa-square-check"></i></a>انجام خرید فلان</li>
+                            <li><a href="#"><i class="fas fa-square-check"></i></a>انجام خرید فلان</li>
+                            <li><a href="#"><i class="fas fa-square-check"></i></a>انجام خرید فلان</li>
+                            <li><a href="#"><i class="fas fa-square-check"></i></a>انجام خرید فلان</li>
+                            <li><a href="#"><i class="fas fa-square-check"></i></a>انجام خرید فلان</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-lg-3">
+                    <div class="box">
+                        <h2><i class="fas fa-square-check"></i>انجام شده ها</h2>
+                        <ul class="list done">
+                            <li><a href="#"><i class="fas fa-trash"></i></a>تست</li>
+                            <li><a href="#"><i class="fas fa-trash"></i></a>تست</li>
+                            <li><a href="#"><i class="fas fa-trash"></i></a>تست</li>
+                            <li><a href="#"><i class="fas fa-trash"></i></a>تست</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+</div>
+
+<?php require_once 'sections/footer.php'; ?>
