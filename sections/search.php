455bbe3d28982947781e346bb3889a65f7f7bba5
Ava <109676929+Ayu5259@users.noreply.github.com>
2025-05-29 22:21:04 +0330

    final version with all updates after phase-2

---
+++ sections/search.php
@@ -0,0 +1,4 @@
+ <form action="search.php" method="get" class="searchbox">
+     <a href="#"><i class="fas fa-magnifying-glass"></i></a>
+     <input type="text" name="search" placeholder="جستجو">
+ </form>
